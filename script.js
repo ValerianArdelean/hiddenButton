c127173da277653c4a6eb8cb0f9cc2f365eea03d
ValerianArdelean <valerian_ardelean@yahoo.com>
2024-02-23 00:41:13 +0200

    Implemented the second requirement:
    To add an input button, and when user inserted a number, will be created more numbers.

--- script.js
+++ script.js
@@ -1,9 +1,23 @@
 let parent = document.querySelector('div');
+let n = 4;
+document.getElementById("Submit").addEventListener("click", createButtons);
+
+function createButtons() {
+    n = parseInt(n) + parseInt(document.getElementById("n").value);
+    console.log(n);
+    for (let i = 4; i < n; ++i) {
+        let button = document.createElement("button");
+        button.classList.add("button");
+        button.id = i;
+        parent.appendChild(button);
+    }
+}
+
 parent.addEventListener("click", displayWinner);
 
 function displayWinner(evt) {
-    let number = Math.floor(Math.random() * 3);
-    for (let i = 0; i < 3; ++i) {
+    let number = Math.floor(Math.random() * n);
+    for (let i = 0; i < n; ++i) {
         let button = parent.children[i];
         if (button.id == evt.target.id) {
             if (button.id == number) {
