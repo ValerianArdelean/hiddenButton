c21a417c7aed8f6c9637f51bdbf583c8384f345a
ValerianArdelean <valerian_ardelean@yahoo.com>
2024-02-22 21:41:39 +0200

    Moved styles to css file
    Also changed default button style

--- script.js
+++ script.js
@@ -6,12 +6,12 @@ function ceva(evt) {
     for (let i = 0; i < 3; ++i) {
         if (parent.children[i].id == evt.target.id) {
             if (parent.children[i].id == number) {
-                parent.children[i].innerHTML = "castigator";
+                parent.children[i].innerText = "castigator";
             } else {
-                parent.children[i].innerHTML = "necastigator";
+                parent.children[i].innerText = "necastigator";
             }
         } else {
-            parent.children[i].innerHTML = "";
+            parent.children[i].innerText = "";
         }
     }
 }
